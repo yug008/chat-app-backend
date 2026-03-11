a031cab65ff50106033376ed2e258a485da9a9bb
Yug Mehta <yug26mehta@gmail.com>
2026-03-12 00:10:34 +0530

    add Vercel URL to CORS config

--- src/main/java/com/chatapp/chat_app_backend/config/SecurityConfig.java
+++ src/main/java/com/chatapp/chat_app_backend/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000","https://chat-app-frontend-hazel-six.vercel.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
